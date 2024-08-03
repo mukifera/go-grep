d2e0d0da89625317402d7c98223a19735e1547a5
Feras Muki <feras.muki@gmail.com>
2024-08-03 19:52:07 -0400

    Implement digit matching

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -36,13 +36,31 @@ func main() {
 	// default exit code is 0 which means success
 }
 
+func isLetter(char rune) bool {
+	if 'a' <= char && char <= 'z' { return true; }
+	if 'A' <= char && char <= 'Z' { return true; }
+	return false
+}
+
+func isDigit(char rune) bool {
+	return '0' <= char && char <= '9'
+}
+
 func matchLine(line []byte, pattern string) (bool, error) {
-	if utf8.RuneCountInString(pattern) != 1 {
+	if utf8.RuneCountInString(pattern) > 2 {
 		return false, fmt.Errorf("unsupported pattern: %q", pattern)
 	}
 
 	var ok bool
-	ok = bytes.ContainsAny(line, pattern)
+	var fun func (rune) bool
+
+	switch {
+	case pattern == "\\d": fun = isDigit; break;
+	case isLetter(rune(pattern[0])): fun = isLetter; break;
+	default: fun = nil; break;
+	}
+
+	ok = bytes.ContainsFunc(line, fun)
 
 	return ok, nil
 }
